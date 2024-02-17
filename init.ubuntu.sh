c7533d5ba34dc574c2793a6bcc7e9f230232bb2c
curelesss <curelesss@gmail.com>
2024-02-17 19:53:13 +0800

    fix remote repo url

--- init.ubuntu.sh
+++ init.ubuntu.sh
@@ -28,7 +28,7 @@ echo -*********************************-
 echo -** Set current repo to ssh     **-
 echo -*********************************-
 
-git remote set-url origin git@github.com:curelesss/init.git
+git remote set-url origin git@github.com:curelesss/init.ubuntu.git
 
 echo -*********************************-
 echo -** Run Ansible Init Play-book  **-
