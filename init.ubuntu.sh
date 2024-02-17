d91bb7070bd291faaea40ff47af0307230aec792
curelesss <curelesss@gmail.com>
2024-02-17 19:27:46 +0800

    first commit

---
+++ init.ubuntu.sh
@@ -0,0 +1,37 @@
+echo -*********************************-
+echo -** Update Ubuntu Mirror List   **-
+echo -*********************************-
+
+sudo rm -f /etc/apt/sources.list
+sudo cp ./mirrorlist/sources.list /etc/apt/
+
+echo -*********************************-
+echo -** Add Ansible Repository      **-
+echo -*********************************-
+
+sudo apt-add-repository ppa:ansible/ansible -y
+
+echo -*********************************-
+echo -** Update and Upgrade Sysytem  **-
+echo -*********************************-
+
+sudo apt update
+sudo apt upgrade -y
+
+echo -*********************************-
+echo -** Install Ansible             **-
+echo -*********************************-
+
+sudo apt install software-properties-common ansible -y
+
+echo -*********************************-
+echo -** Set current repo to ssh     **-
+echo -*********************************-
+
+git remote set-url origin git@github.com:curelesss/init.git
+
+echo -*********************************-
+echo -** Run Ansible Init Play-book  **-
+echo -*********************************-
+
+ansible-playbook init.play-book.yml
